8bdc4b08ddcac0c5682a02d46b1b76d8c794e04b
김민정 <zoek2100@gmail.com>
2021-11-19 23:28:33 -0500

    feat : create a file of kmers

--- scripts/kmers_list.py
+++ scripts/kmers_list.py
@@ -9,3 +9,12 @@ class kmers_list:
             self.li.append(seq[i: i + num])
         return self.li
 
+    def create_list(self, filename):
+        count = 1
+        #.fa format 
+        with open(filename, 'w') as f: 
+            for k in self.li:
+                f.write('>read' + str(count) + '\n')
+                count = count + 1
+                f.write('%s\n' % k)
+
